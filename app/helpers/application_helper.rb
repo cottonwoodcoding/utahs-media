c7ad934148791987083e73195ea796bc9412527c
Jake Sorce <jakesorce@gmail.com>
2015-02-11 20:29:02 -0700

    style and bottom brand logos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_class(*controllers)
+    if params[:action] == 'index'
+      controllers.include?(params[:controller]) ? 'active' : ''
+    else
+      controllers.first == ("#{params[:controller]}/#{params[:action]}") ? 'active' : ''
+    end
+  end
 end
