71909d81c2303addaa97419ad96a91e701937cfa
dave <dave@instructure.com>
2015-02-25 21:28:53 -0800

    gallery changes

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -6,8 +6,10 @@ class GalleryController < ApplicationController
   def index
     @album_hash = {}
     image_shack_albums.each do |album|
+      binding.pry
       next if album['public'] == false
       next if album['images'].length == 0
+      next if album['id'] == ENV['image_shack_main_album'].to_i
       @album_hash[album['id']] = { name: album['title'], images: [] }
       album['images'].each do |img|
         image = image_shack_image_src(img['server'], img['filename'])
