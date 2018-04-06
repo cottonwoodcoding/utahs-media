318a41a519db0822420b53ef975f1b84912a0ebe
Jake Sorce <jake.sorce@podium.com>
2018-04-05 21:27:54 -0600

    rebrand

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,10 +1,10 @@
 class FeedbackMailer < ActionMailer::Base
-  default from: "feedback@structuredmediasystems.com"
+  default from: "feedback@utahsmedia.com"
 
   def send_feedback(email, name, content)
     @email = email
     @name = name
     @content = content
-    mail(to: ENV['SEND_TO'], subject: "#{name} has a message for Structured Media Systems.")
+    mail(to: ENV['SEND_TO'], subject: "#{name} has a message for UtahLED.")
   end
 end
